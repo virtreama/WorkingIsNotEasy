b4b29f5c33a96af973e61dd52f2fbd8a83e3db3c
GenMF <marcfriedrich85@gmail.com>
2022-08-12 13:20:23 +0200

    clean batch
    - clean batch file added to remove all not needed files

---
+++ Clean.bat
@@ -0,0 +1,11 @@
+:: remove editor ue5 project binaries, intermediate and build folder and files
+cd WorkingIsNotEasy
+rmdir .vs /q /s
+rmdir Binaries /q /s
+rmdir Build /q /s
+rmdir DerivedDataCache /q /s
+rmdir Intermediate /q /s
+rmdir Platforms /q /s
+rmdir Saved /q /s
+rmdir Script /q /s
+del /q /s /f WorkingIsNotEasy.sln

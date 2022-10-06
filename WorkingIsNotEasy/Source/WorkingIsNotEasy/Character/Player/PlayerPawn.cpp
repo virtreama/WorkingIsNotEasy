87499884e474f12b468cd8cc39bd7050ce11bf45
GenMF <marcfriedrich85@gmail.com>
2022-10-06 10:43:35 +0200

    setup player components
    - player main class added
    - player components set up
    - player blueprint created

---
+++ WorkingIsNotEasy/Source/WorkingIsNotEasy/Character/Player/PlayerPawn.cpp
@@ -0,0 +1,88 @@
+#pragma region include project
+#include "PlayerPawn.h"
+#pragma endregion
+
+#pragma region include engine
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "MotionControllerComponent.h"
+#pragma endregion
+
+#pragma region constructor
+// constructor
+APlayerPawn::APlayerPawn()
+{
+	// enable update every frame
+	PrimaryActorTick.bCanEverTick = true;
+
+	// create root scene component
+	USceneComponent* pRoot = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	RootComponent = pRoot;
+
+	// create camera component and attach to root
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(pRoot);
+
+	// create capsule component and  attach to root
+	Collision = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Collision"));
+	Collision->SetupAttachment(pRoot);
+
+	// create IK skeletal mesh component and attach to root
+	IK = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("IK"));
+	IK->SetupAttachment(pRoot);
+
+	// create head skeletal mesh component and attach to IK
+	Head = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Head"));
+	Head->SetupAttachment(IK);
+
+	// create arms skeletal mesh component and attach to IK
+	Arms = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Arms"));
+	Arms->SetupAttachment(IK);
+
+	// create left hand skeletal mesh component and attach to IK
+	LeftHand = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("LeftHand"));
+	LeftHand->SetupAttachment(IK);
+
+	// create right hand skeletal mesh component and attach to IK
+	RightHand = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("RightHand"));
+	RightHand->SetupAttachment(IK);
+
+	// create body skeletal mesh component and attach to IK
+	Body = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Body"));
+	Body->SetupAttachment(IK);
+
+	// create left hand motion controller component and attach to root
+	LeftHandMotionController = CreateDefaultSubobject<UMotionControllerComponent>(TEXT("LeftMotionController"));
+	LeftHandMotionController->SetupAttachment(pRoot);
+
+	// create right hand motion controller component and attach to root
+	RightHandMotionController = CreateDefaultSubobject<UMotionControllerComponent>(TEXT("RightMotionController"));
+	RightHandMotionController->SetupAttachment(pRoot);
+
+	// create left waist bag component and attach to root
+	LeftWaistBag = CreateDefaultSubobject<USceneComponent>(TEXT("LeftWaistBag"));
+	LeftWaistBag->SetupAttachment(pRoot);
+
+	// create right waist bag component and attach to root
+	RightWaistBag = CreateDefaultSubobject<USceneComponent>(TEXT("RightWaistBag"));
+	RightWaistBag->SetupAttachment(pRoot);
+}
+#pragma endregion
+
+#pragma region public override function
+// update every frame
+void APlayerPawn::Tick(float DeltaTime)
+{
+	// parent update every frame
+	Super::Tick(DeltaTime);
+}
+#pragma endregion
+
+#pragma region protected override function
+// called at begin play
+void APlayerPawn::BeginPlay()
+{
+	// parent begin play
+	Super::BeginPlay();
+}
+#pragma endregion
---
+++ WorkingIsNotEasy/Source/WorkingIsNotEasy/Character/Player/PlayerPawn.h
@@ -0,0 +1,123 @@
+#pragma once
+
+#pragma region include engine
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#pragma endregion
+
+#pragma region include project
+#include "PlayerPawn.generated.h"
+#pragma endregion
+
+#pragma region forward decleration
+class UCameraComponent;
+class UCapsuleComponent;
+class UMotionControllerComponent;
+#pragma endregion
+
+UCLASS()
+/// <summary>
+/// player class
+/// </summary>
+class WORKINGISNOTEASY_API APlayerPawn : public APawn
+{
+	GENERATED_BODY()
+
+public:
+#pragma region constructor
+	/// <summary>
+	/// constructor
+	/// </summary>
+	APlayerPawn();
+#pragma endregion
+
+#pragma region public override function
+	/// <summary>
+	/// update every frame
+	/// </summary>
+	/// <param name="DeltaTime">time since last frame</param>
+	virtual void Tick(float DeltaTime) override;
+#pragma endregion
+
+#pragma region public UPROPERTY
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Player Pawn")
+	/// <summary>
+	/// main camera component
+	/// </summary>
+	UCameraComponent* Camera = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Player Pawn")
+	/// <summary>
+	/// collision capsule component
+	/// </summary>
+	UCapsuleComponent* Collision = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Player Pawn")
+	/// <summary>
+	/// IK target skeletal mesh component
+	/// </summary>
+	USkeletalMeshComponent* IK = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Player Pawn")
+	/// <summary>
+	/// head visual skeletal mesh component
+	/// </summary>
+	USkeletalMeshComponent* Head = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Player Pawn")
+	/// <summary>
+	/// arms visual skeletal mesh component
+	/// </summary>
+	USkeletalMeshComponent* Arms = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Player Pawn")
+	/// <summary>
+	/// left hand visual skeletal mesh component
+	/// </summary>
+	USkeletalMeshComponent* LeftHand = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Player Pawn")
+	/// <summary>
+	/// right hand visual skeletal mesh component
+	/// </summary>
+	USkeletalMeshComponent* RightHand = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Player Pawn")
+	/// <summary>
+	/// body visual skeletal mesh component
+	/// </summary>
+	USkeletalMeshComponent* Body = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Player Pawn")
+	/// <summary>
+	/// left hand motion controller component
+	/// </summary>
+	UMotionControllerComponent* LeftHandMotionController = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Player Pawn")
+	/// <summary>
+	/// right hand motion controller component
+	/// </summary>
+	UMotionControllerComponent* RightHandMotionController = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Player Pawn")
+	/// <summary>
+	/// left waist bag location component
+	/// </summary>
+	USceneComponent* LeftWaistBag = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Player Pawn")
+	/// <summary>
+	/// right waist bag location component
+	/// </summary>
+	USceneComponent* RightWaistBag = nullptr;
+#pragma endregion
+
+protected:
+#pragma region protected override function
+	/// <summary>
+	/// called at begin play
+	/// </summary>
+	virtual void BeginPlay() override;
+#pragma endregion
+};
